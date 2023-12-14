642d9abec3ccb9d803ac2eaa7064dbd71a408ec0
가동재 <masterforce999@naver.com>
2023-12-14 23:11:36 +0900

    1일차 문제 풀이

---
+++ 내연습용 문제풀이/1/js/script.js
@@ -0,0 +1,43 @@
+var 사람 = {
+  name: "손흥민",
+  sayHi() {
+    return console.log("안녕 나는 " + this.name);
+  },
+};
+
+사람.sayHi(); //안녕 나는 손흥민
+
+var 자료 = {
+  data: [1, 2, 3, 4, 5],
+  전부더하기() {
+    let sum = 0;
+    for (i = 0; i < this.data.length; i++) {
+      sum += this.data[i];
+    }
+
+    return console.log("2번:::", sum);
+  },
+};
+자료.전부더하기();
+
+const button = document.getElementById("button");
+button.addEventListener("click", function () {
+  setTimeout(() => {
+    console.log(this.innerHTML);
+  }, 1000);
+});
+
+for (let i = 0; i < 5; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, i * 1000);
+}
+
+var 버튼들 = document.querySelectorAll(".btn");
+var 모달창들 = document.querySelectorAll("div");
+
+for (let i = 0; i < 3; i++) {
+  버튼들[i].addEventListener("click", function () {
+    모달창들[i].style.display = "block";
+  });
+}
