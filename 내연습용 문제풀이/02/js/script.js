01aa63f60ca47651d779d1b98bdb4b998bd49864
가동재 <masterforce999@naver.com>
2023-12-18 08:44:07 +0900

    알고리즘 완료

--- 내연습용 문제풀이/02/js/script.js
+++ 내연습용 문제풀이/02/js/script.js
@@ -58,3 +58,16 @@ function solution4(n) {
 }
 
 solution4(1000);
+
+// 문자열 str1, str2가 매개변수로 주어집니다. str1 안에 str2가 있다면 1을 없다면 2를 return하도록 solution 함수를 완성해주세요.
+function solution5(str1, str2) {
+  var answer = 0;
+
+  if (str1.includes(str2)) {
+    return (answer = 1);
+  } else {
+    return (answer = 2);
+  }
+}
+
+solution5("ab6CDE443fgh22iJKlmn1o", "6CD");
