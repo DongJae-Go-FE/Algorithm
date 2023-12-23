80d4d71ecfb4bbcc4560cce86e4f476571d0b6bc
가동재 <masterforce999@naver.com>
2023-12-23 23:36:30 +0900

    문제풀이 진행중

--- 내연습용 문제풀이/02/js/script.js
+++ 내연습용 문제풀이/02/js/script.js
@@ -115,3 +115,15 @@ function solution8(n) {
 }
 
 solution8(144);
+
+function solution9(slice, n) {
+  var answer = 0;
+  if (n % slice != 0) {
+    answer = n / slice + 1;
+  } else {
+    answer = n / slice;
+  }
+  return parseInt(answer);
+}
+
+solution9(7, 10);
