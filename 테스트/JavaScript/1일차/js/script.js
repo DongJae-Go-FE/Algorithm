cc55f4a11cafbd2b673cb549ce9cd87d379ebf6b
가동재 <masterforce999@naver.com>
2024-06-15 00:46:09 +0900

    async test

--- 테스트/JavaScript/1일차/js/script.js
+++ 테스트/JavaScript/1일차/js/script.js
@@ -8,8 +8,27 @@ const sync = () => {
 
 const async = async () => {
   console.log("동기:::", "1");
-  await setTimeout(() => {
-    console.log("동기:::", "2");
-  }, 2000);
+  await new Promise((resolve) =>
+    setTimeout(() => {
+      console.log("동기:::", "2");
+      resolve();
+    }, 2000)
+  );
+
   console.log("동기:::", "3");
 };
+
+new Promise((resolve, err) => {
+  console.log("프로미스1");
+  err();
+})
+  .then(() => {
+    console.log("프로미스2");
+  })
+  .then(() => {
+    console.log("프로미스3");
+  }).catch(()=>{
+    alert("에러")
+  }).finally(()=>{
+    alert("파이널리")
+  });
