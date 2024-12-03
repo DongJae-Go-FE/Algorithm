046c87d113ab6cd6098873270df70c52e0047bbf
가동재 <130569283+DongJae-Go-FE@users.noreply.github.com>
2024-12-03 10:36:35 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,16 @@
+function solution(numbers, n) {
+    var answer = 0;
+    for(let i= 0; i< numbers.length;i++){
+
+        if( n > answer){
+            answer += numbers[i]
+        }else if(n === answer){
+            answer += numbers[i]
+        }else{
+            answer
+        }
+
+    }
+    
+    return answer;
+}
